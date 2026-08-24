8b1fbf098cd040033a0af750a251e959c856adaa
nowwa wu <2543775850@qq.com>
2026-08-24 10:01:36 +0800

    fix: normalize bizdate placeholder in examples/sql/dwd_demo_order_line_di.etl.sql

--- examples/sql/dwd_demo_order_line_di.etl.sql
+++ examples/sql/dwd_demo_order_line_di.etl.sql
@@ -1,4 +1,4 @@
-INSERT OVERWRITE TABLE dwd_demo_order_line_di PARTITION(ds='\${bizdate}')
+INSERT OVERWRITE TABLE dwd_demo_order_line_di PARTITION(ds='${bizdate}')
 SELECT
     order_line_id,
     shop_id,
@@ -6,4 +6,4 @@ SELECT
     CAST(paid_amount AS DECIMAL(18, 4)) AS paid_amount,
     CAST(paid_at AS DATETIME) AS paid_at
 FROM ods_demo_order_line_di
-WHERE ds = '\${bizdate}';
+WHERE ds = '${bizdate}';
